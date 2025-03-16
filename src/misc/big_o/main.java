7091a5f4c881ea258dd2736ad26d4e718997b64a
shravan <shravankumarc87@gmail.com>
2025-03-16 13:01:20 +0530

    first commit

--- src/misc/big_o/main.java
+++ src/misc/big_o/main.java
@@ -3,17 +3,14 @@ package misc.big_o;
 public class main {
     public static void main(String[] args) {
 
-        PrintItems(10);
+        PrintItems(10,12);
     }
 
-    static void PrintItems(int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+    static void PrintItems(int a, int b) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
                 System.out.println(i + " " + j);
             }
-        }
-        for (int k = 0; k < n; k++) {
-            System.out.println(k);
         }
     }
 }
