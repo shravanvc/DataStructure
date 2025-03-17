9d32f883986c9652bff69009fb5ff491c70e9618
shravan <shravankumarc87@gmail.com>
2025-03-17 22:55:08 +0530

    first commit

--- src/misc/big_o/main.java
+++ src/misc/big_o/main.java
@@ -1,16 +1,16 @@
-package misc.big_o;
-
-public class main {
-    public static void main(String[] args) {
-
-        PrintItems(10,12);
-    }
-
-    static void PrintItems(int a, int b) {
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                System.out.println(i + " " + j);
-            }
-        }
-    }
-}
+//package misc.big_o;
+//
+//public class main {
+//    public static void main(String[] args) {
+//
+//        PrintItems(10,12);
+//    }
+//
+//    static void PrintItems(int a, int b) {
+//        for (int i = 0; i < a; i++) {
+//            for (int j = 0; j < b; j++) {
+//                System.out.println(i + " " + j);
+//            }
+//        }
+//    }
+//}
