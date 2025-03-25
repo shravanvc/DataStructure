90163047ac9ca767a8047faa3a1f2ab172f093fd
shravan <shravankumarc87@gmail.com>
2025-03-25 22:35:26 +0530

    first commit

--- src/Pointers/Map.java
+++ src/Pointers/Map.java
@@ -1,18 +1,18 @@
-package Pointers;
-
-import java.util.HashMap;
-
-public class Map {
-    public static void main(String[] args) {
-        HashMap<String, Integer> map1 = new HashMap<>();
-        HashMap<String, Integer> map2 = new HashMap<>();
-
-        map1.put("value",11);
-        map2 = map1;
-
-        map1.put("value",22);
-
-        System.out.println(map1);
-        System.out.println(map2);
-    }
-}
+//package Pointers;
+//
+//import java.util.HashMap;
+//
+//public class Map {
+//    public static void main(String[] args) {
+//        HashMap<String, Integer> map1 = new HashMap<>();
+//        HashMap<String, Integer> map2 = new HashMap<>();
+//
+//        map1.put("value",11);
+//        map2 = map1;
+//
+//        map1.put("value",22);
+//
+//        System.out.println(map1);
+//        System.out.println(map2);
+//    }
+//}
